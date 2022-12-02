0a2f20de43fd1be3df505840e10fecc6d28a847d
Arnon <arnon.eilat@khealth.ai>
2022-12-02 12:21:24 +0200

    Add K Health and Papaya Global

--- static-generator/after-build.js
+++ static-generator/after-build.js
@@ -1,15 +1,15 @@
-const minify = require("html-minifier").minify;
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
-const CleanCSS = require("clean-css");
+const minify = require('html-minifier').minify;
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const CleanCSS = require('clean-css');
 
-const log = (content) => console.log(chalk.blue("[build]"), content);
-const readFile = (path) => fs.readFileSync(path, "utf8");
-const writeFile = (path, content) => fs.writeFileSync(path, content, "utf8");
+const log = (content) => console.log(chalk.blue('[build]'), content);
+const readFile = (path) => fs.readFileSync(path, 'utf8');
+const writeFile = (path, content) => fs.writeFileSync(path, content, 'utf8');
 
 const minifyHtml = function (htmlInput) {
-  if (typeof htmlInput !== "string") {
+  if (typeof htmlInput !== 'string') {
     throw new TypeError("Bad arguments supply: 'htmlInput' should be string");
   }
   const options = {
@@ -23,18 +23,18 @@ const minifyHtml = function (htmlInput) {
   return output;
 };
 
-log("Reading html content");
-const indexHtmlPath = path.join(__dirname, "/../index.html");
+log('Reading html content');
+const indexHtmlPath = path.join(__dirname, '/../index.html');
 const htmlContent = readFile(indexHtmlPath);
-log("Minifying html");
+log('Minifying html');
 const minifyedHtml = minifyHtml(htmlContent);
-log("Writing html content to " + indexHtmlPath);
+log('Writing html content to ' + indexHtmlPath);
 writeFile(indexHtmlPath, minifyedHtml);
 
-log("Removing unnecessary files");
+log('Removing unnecessary files');
 const redundantfiles = [
-  path.join(__dirname, "/../styles/bootstrap/.csscomb.json"),
-  path.join(__dirname, "/../styles/bootstrap/.csslintrc"),
+  path.join(__dirname, '/../styles/bootstrap/.csscomb.json'),
+  path.join(__dirname, '/../styles/bootstrap/.csslintrc'),
 ];
 redundantfiles.forEach((filePath) => fs.unlink(filePath, () => {}));
 
@@ -45,11 +45,11 @@ const minifyCss = function minifyCss(path) {
 };
 
 const cssFilesToClean = [
-  path.join(__dirname, "/../styles/bootstrap/bootstrap.css"),
-  path.join(__dirname, "/../styles/bootstrap/theme.css"),
-  path.join(__dirname, "/../styles/print.css"),
-  path.join(__dirname, "/../styles/style.css"),
+  path.join(__dirname, '/../styles/bootstrap/bootstrap.css'),
+  path.join(__dirname, '/../styles/bootstrap/theme.css'),
+  path.join(__dirname, '/../styles/print.css'),
+  path.join(__dirname, '/../styles/style.css'),
 ];
 
-log("Minifying css files");
+log('Minifying css files');
 cssFilesToClean.forEach((filePath) => minifyCss(filePath));
